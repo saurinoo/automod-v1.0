7b7ac328ff516973f6519b675e100ee34e9dae9e
saurino <61811183+saurinoo@users.noreply.github.com>
2023-03-12 11:28:59 +0100

    Config automod system

--- commands/autmod.js
+++ commands/autmod.js
@@ -31,6 +31,23 @@ module.exports = {
         )
         .addSubcommand((subcommand) =>
             subcommand
+                .setName('set-timeout')
+                .setDescription('Set timout duration')
+                .addStringOption((options) =>
+                    options
+                        .setName('duration')
+                        .setDescription('Choice duration for timeout')
+                        .addChoices(
+                            { name: '1d', value: '1d' },
+                            { name: '12h', value: '12h' },
+                            { name: '3h', value: '3h' },
+                            { name: '1h', value: '1h' },
+                            { name: '30m', value: '30m' },
+                            { name: '10m (default)', value: '10m' }
+                        ))
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
                 .setName('plugin')
                 .setDescription('Select a plugin to enable with automod')
                 .addStringOption((options) =>
@@ -47,12 +64,13 @@ module.exports = {
                         )
                 )
         ),
-    async execute(interaction, client) {
+    async execute(interaction) {
         const { guildId, options, guild } = interaction;
 
         const subcommand = options.getSubcommand();
         const channel = options.getChannel('channel');
         const plugin = options.getString('plugin');
+        const timeout = options.getString('duration');
 
         const prefix = '\`Automod\`'
 
@@ -67,9 +85,11 @@ module.exports = {
                     interaction.reply({ content: ` logs channel succesfull setup to ${channel}.`, ephemeral: true });
 
                     try {
-                        guild.channels.cache.get(channel.id).send({ embeds: [new EmbedBuilder().setDescription(`\`✅\` This is the autmod logs channel.
+                        guild.channels.cache.get(channel.id).send({
+                            embeds: [new EmbedBuilder().setDescription(`\`✅\` This is the autmod logs channel.
                         
-                        > In this channel all automod reports will\n> be sent, it is recommended to make this\n> channel visible only to moderators.`).setColor('Grey')]});
+                        > In this channel all automod reports will\n> be sent, it is recommended to make this\n> channel visible only to moderators.`).setColor('Grey')]
+                        });
                     } catch (error) {
                         return
                     }
@@ -118,6 +138,18 @@ module.exports = {
             })
         }
 
+        if (subcommand === 'set-timeout') {
+            automodSchema.findOne({ guildId: guildId }, async (err, data) => {
+                if (err) throw err;
+                if (!data) return interaction.reply({ content: `No ${prefix} setup find, use </automod setup:1084256969789022338> to setup system.`, ephemeral: true });
+                if (data) {
+                    data.timeout = timeout;
+                    data.save();
+                    interaction.reply({ content: `${prefix} | timeout duration has been updated to **${timeout}**`, ephemeral: true });
+                }
+            })
+        }
+
         if (subcommand === 'plugin') {
             automodSchema.findOne({ guildId: guildId }, async (err, data) => {
                 if (err) throw err;
@@ -151,7 +183,7 @@ module.exports = {
                     data.save();
 
                     interaction.reply({ content: `> ${prefix} | Now the __${pname}__ plugin has been **${data.plugins[plugin] == false ? 'disabled' : 'enabled'}**.`, ephemeral: true });
-                    
+
                 }
             })
         }
